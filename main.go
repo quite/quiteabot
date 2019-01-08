2f70b11da54f0cb5f331d33986a825fdb9a0655a
Daniel Lublin <daniel@lublin.se>
2019-01-08 21:15:22 +0100

    Use config file etc

--- main.go
+++ main.go
@@ -9,33 +9,49 @@ import (
 
 	xmpp "github.com/mattn/go-xmpp"
 	tb "gopkg.in/tucnak/telebot.v2"
+	yaml "gopkg.in/yaml.v2"
 )
 
-var server = "picapica.lublin.se:5222"
-var username = "quiteabot@lublin.se"
-var password = "zDhN9F2EiYm79HZOpZFDP594V"
-var status = "xa"
-var statusMessage = "woot"
-var debug = false
-var session = false //not server session
+type Config struct {
+	XMPPServer    string
+	XMPPUser      string
+	XMPPPass      string
+	XMPPTarget    string
+	TelegramToken string
+	TelegramUsers map[string]int
+}
 
-func serverName(host string) string {
-	return strings.Split(host, ":")[0]
+func parseconfig(filename string) (conf *Config, err error) {
+	contents, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return
+	}
+	conf = new(Config)
+	if err = yaml.Unmarshal(contents, &conf); err != nil {
+		return
+	}
+	return
 }
 
 func main() {
 	var xmppc *xmpp.Client
 	var err error
+
+	conf, err := parseconfig("quiteabot.yaml")
+	if err != nil {
+		panic(err)
+	}
+
 	options := xmpp.Options{
-		Host:          server,
-		User:          username,
-		Password:      password,
+		Host:          conf.XMPPServer,
+		User:          conf.XMPPUser,
+		Password:      conf.XMPPPass,
 		NoTLS:         true,
 		StartTLS:      true,
-		Debug:         debug,
-		Session:       session,
-		Status:        status,
-		StatusMessage: statusMessage,
+		Debug:         false,
+		Session:       false, // no server session
+		Status:        "xa",
+		StatusMessage: "i'm a bot",
 	}
 
 	xmppc, err = options.NewClient()
@@ -43,12 +59,8 @@ func main() {
 		log.Fatal(err)
 	}
 
-	token, err := ioutil.ReadFile("quiteabot.telegramtoken")
-	if err != nil {
-		panic(err)
-	}
 	tbc, err := tb.NewBot(tb.Settings{
-		Token: strings.TrimSpace(string(token)),
+		Token: conf.TelegramToken,
 		// // You can also set custom API URL. If field is empty it equals to "https://api.telegram.org"
 		// URL:    "http://195.129.111.17:8012",
 		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
@@ -62,7 +74,7 @@ func main() {
 		// 780111986 me
 		msg := fmt.Sprintf("%s%s<%d>: %s", m.Sender.FirstName, m.Sender.LastName, m.Sender.ID, m.Text)
 		fmt.Printf("---\nfrom: %s\n", msg)
-		xmppc.Send(xmpp.Chat{Remote: "daniel@lublin.se", Type: "chat", Text: msg})
+		xmppc.Send(xmpp.Chat{Remote: conf.XMPPTarget, Type: "chat", Text: msg})
 		fmt.Printf("relayed to xmpp\n")
 	})
 
@@ -75,25 +87,24 @@ func main() {
 			switch v := chat.(type) {
 			case xmpp.Chat:
 				fmt.Printf("---\nfrom: %s\n", v.Remote)
-				if !strings.HasPrefix(v.Remote, "daniel@lublin.se") {
+				if !strings.HasPrefix(v.Remote, conf.XMPPTarget) {
 					fmt.Printf("ignored\n")
 					continue
 				}
 				usermsg := strings.SplitN(v.Text, ":", 2)
 				if len(usermsg) < 2 {
-					xmppc.Send(xmpp.Chat{Remote: "daniel@lublin.se", Type: "chat", Text: "expected: user:the msg"})
+					xmppc.Send(xmpp.Chat{Remote: conf.XMPPTarget, Type: "chat", Text: "expected: user:the msg"})
 					fmt.Printf("wrong format: %s\n", v.Text)
 					continue
 				}
-				users := map[string]int{"me": 780111986}
-				userid := users[usermsg[0]]
+				userid := conf.TelegramUsers[usermsg[0]]
 				if userid == 0 || len(usermsg[1]) == 0 {
-					xmppc.Send(xmpp.Chat{Remote: "daniel@lublin.se", Type: "chat", Text: "unknown user"})
+					xmppc.Send(xmpp.Chat{Remote: conf.XMPPTarget, Type: "chat", Text: "unknown user"})
 					fmt.Printf("unknown user: %s (or empty msg)\n", usermsg[0])
 					continue
 				}
 				tbc.Send(&tb.User{ID: userid}, usermsg[1], tb.NoPreview)
-				fmt.Printf("relayed to teleg\n")
+				fmt.Printf("relayed to <%d>\n", userid)
 				// case xmpp.Presence:
 				// 	fmt.Println(v.From, v.Show)
 			}
